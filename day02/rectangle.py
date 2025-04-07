664afd0f44b79880ef04d3833a8fb4978c3e4bb3
AriFinn97 <aaron.finn@weizmann.ac.il>
2025-04-07 15:15:28 +0300

    Update rectangle.py

--- day02/rectangle.py
+++ day02/rectangle.py
@@ -1,1 +1,7 @@
 
+height = float(input( "please enter the height of your rectangle:"))
+width = float(input( "please enter the width of your rectangle:"))
+area = height * width
+perimeter = 2 * ( height + width )
+print (f" your rectangle has a area of {area:.2f} and a perimeter of {perimeter:.2f}")
+
