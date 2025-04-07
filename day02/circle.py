a9605294f47fdb9e037488491d567a69882324e6
AriFinn97 <aaron.finn@weizmann.ac.il>
2025-04-07 14:38:01 +0300

    Create circle.py

---
+++ day02/circle.py
@@ -0,0 +1,9 @@
+
+measurements = 
+Write a program called circle.py that will ask the user for the radius of the circle and will print the area and the circumeference of the circle.
+
+Write a program called rectangle.py that will ask the user for height and width and calculate the area and the perimeter of the rectangle.
+
+You will have to use the int() or the float() function to convert tha data you received from the user to an int or float in your program.
+
+Once you are done, open an issue on our repository in whch the subject includes your full name and the number of the assignment so the TAs will be able to handle it. Include a link to the repository of your assignments.
