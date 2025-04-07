258a2537c7f91b0e2c61779fef2aa6445e77967e
AriFinn97 <aaron.finn@weizmann.ac.il>
2025-04-07 15:00:46 +0300

    Update circle.py

--- day02/circle.py
+++ day02/circle.py
@@ -1,9 +1,12 @@
+radius = float(input("Please enter the radius of the circle: "))
+circumference = 2 * 3.14 * radius    
+area = 3.14 * radius * radius 
+print(f"Your circle's circumference is {circumference:.2f} and area is {area:.2f}")
 
-measurements = 
-Write a program called circle.py that will ask the user for the radius of the circle and will print the area and the circumeference of the circle.
 
 Write a program called rectangle.py that will ask the user for height and width and calculate the area and the perimeter of the rectangle.
 
 You will have to use the int() or the float() function to convert tha data you received from the user to an int or float in your program.
 
-Once you are done, open an issue on our repository in whch the subject includes your full name and the number of the assignment so the TAs will be able to handle it. Include a link to the repository of your assignments.
+Once you are done, open an issue on our repository in whch the subject includes your full name and the number of the assignment so the TAs will 
+be able to handle it. Include a link to the repository of your assignments.
