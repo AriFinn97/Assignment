ac28d076e6907739499f020b1659a5d10c116e23
AriFinn97 <aaron.finn@weizmann.ac.il>
2025-04-23 17:05:44 +0300

    Create guess0.py

---
+++ day_03/guess0.py
@@ -0,0 +1,7 @@
+number = 7
+guess = int(input("please guess a number from 1 to 10"))
+if guess == number:
+            print ("yay you guessed the right nmuber!")
+else: 
+         print ("oof you guessed the wrong number :(")    
+          
