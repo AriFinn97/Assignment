b38c6f5fdde80b68cccde15557ec4dfa69ec6730
AriFinn97 <aaron.finn@weizmann.ac.il>
2025-05-18 20:00:54 +0300

    Update number_guessing_game_1.py

--- day04/number_guessing_game_1.py
+++ day04/number_guessing_game_1.py
@@ -1,1 +1,15 @@
+import random
 
+def main():
+    think = random.randint(1, 21)
+    while True:
+        guess = int(input("Guess a number between 1 and 20: "))
+        if guess < think:
+            print("Your guess is too low.")
+        elif guess > think:
+            print("Your guess is too high.")
+        else:
+            print("Congratulations! You guessed it.")
+            break
+
+main()
